9d92920144bcc8b56c4bec8f58a40232db18108b
verdantkyber <kingjalexa@gmail.com>
2019-06-07 23:23:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber(line, name){
   line.push(name)
-  return line
-  console.log (`Welcome, ${name}. You are number ${line.length+1} in line.`)
+  return `Welcome, ${name}. You are number ${line.length+1} in line.`
 }
