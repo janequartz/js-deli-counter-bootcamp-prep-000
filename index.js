d013490f817baf8d4bbdddbe666680c794e81601
verdantkyber <kingjalexa@gmail.com>
2019-06-07 23:44:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function nowServing(katzDeliLine){
 function currentLine(line){
   if (line.length > 0){
     let str = "The line is currently: "
-    for (let i = 1; i < line.length -1; i++){
-      str += `${i}. ${line[i]} ` 
+    for (let i = 0; i <= line.length -1; i++){
+      str += `${i+1}. ${line[i]} ` 
     }
     str += `${line.length}. ${line[line.length]}`
     return str
