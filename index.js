97d21b7c99fa616159f3025fc5f3e5c2524cc863
verdantkyber <kingjalexa@gmail.com>
2019-06-07 23:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber(line, name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
+    return katzDeliLine[0]
     katzDeliLine.shift()
   }
   else return "There is nobody waiting to be served!"
