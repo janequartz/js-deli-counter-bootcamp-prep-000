f5d130b62c5c216c1a6392974c22ac2fb7c0f15b
verdantkyber <kingjalexa@gmail.com>
2019-06-07 23:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line){
     for (let i = 0; i < line.length -1; i++){
       str += `${i+1}. ${line[i]} ` 
     }
-    str += `${line.length}. ${line[line.length-1]}`
+    str += `${line.length}. ${line[line.length-1]},`
     return str
   }
   else return "The line is currently empty."
